012cd69a3b2c04610e348b8ac4660bfa80b00c39
Kathy Hudson <khitx@icloud.com>
2018-01-30 16:01:56 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,17 @@
 class MP3Importer
-  attr_accessor :path
+  attr_accessor :path :files
 
   def initialize(path)
     @path = path
+    @files = []
   end
 
   def files
-
+    Dir["#{@path}*.mp3"]
   end
+  test_music_path = "./spec/fixtures/mp3s"
+  music_importer = MP3Importer.new(test_music_path)
+
+  expect(music_importer.files.size).to eq(4)
 
 end
